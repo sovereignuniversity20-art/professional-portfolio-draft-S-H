861c73f9c2860a2f93634e8449c4adc3b5470cee
Hero <sovereign.university20@gmail.com>
2026-06-14 00:36:33 -0500

    page 3 updated with pop-up detailing education experience

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const animatedElements = document.querySelectorAll('animate-on-scroll');
+const classRoom = document.querySelectorAll('.soccer-ball, .bookshelf, .plants, .board');
+const popOver = document.querySelectorAll('.popover');
+
+classRoom.forEach(function(hotspot) {
+    const popOver = hotspot.querySelector('.popover');
+    hotspot.addEventListener('click', function() {
+    popOver.classList.toggle('visible');
+});
+});
